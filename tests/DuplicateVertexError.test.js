8c1c5c756e5c207899e5b2601aa02a29aea09056
jimmcgaw <jpmcgaw@gmail.com>
2016-06-21 14:14:20 -0700

    custom error class test assertions (yep, added after code, this isn't pure TDD :) )

--- tests/DuplicateVertexError.test.js
+++ tests/DuplicateVertexError.test.js
@@ -1,5 +1,16 @@
+import { expect } from 'chai';
+
 import DuplicateVertexError from '../src/DuplicateVertexError';
 
 describe('DuplicateVertexError', function () {
-  
+  let error;
+
+  beforeEach(function () {
+    error = new DuplicateVertexError("Cannot add duplicate to graph");
+  });
+
+  it('has the correct error message', function () {
+    expect(error.message).to.equal("Cannot add duplicate to graph");
+    expect(error.name).to.equal("DuplicateVertexError");
+  });
 })
